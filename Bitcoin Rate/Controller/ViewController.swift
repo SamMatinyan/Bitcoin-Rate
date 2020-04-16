841125da5a6285e290abe1abe2e3d769e88e114d
Sam Matinyan <sam.matinyan99@gmail.com>
2020-04-16 23:05:43 +0400

    Add Tab Bar and UI improvements.

--- Bitcoin Rate/Controller/ViewController.swift
+++ Bitcoin Rate/Controller/ViewController.swift
@@ -13,24 +13,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var ethLabel: UILabel!
     @IBOutlet weak var xrpLabel: UILabel!
     
-    var bitcoinManager = BitcoinManager()
-    var curr = ""
+    private var bitcoinManager = BitcoinManager()
+    private let activityView = UIActivityIndicatorView(style: .medium)
+    private var curr = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        showActivityIndicatory()
+
         bitcoinManager.delegate = self
         bitcoinManager.fetchPrice(currency: "GBP")
         curr = "￡"
     }
     
-    
-    @IBAction func infoPressed(_ sender: UIButton) {
-        performSegue(withIdentifier: "ideSeg", sender: self)
-    }
-    
-    
     @IBAction func currencyDidChange(_ sender: UISegmentedControl) {
+        showActivityIndicatory()
         switch sender.selectedSegmentIndex {
         case 0:
             bitcoinManager.fetchPrice(currency: "GBP")
@@ -55,11 +52,19 @@ extension ViewController: BitcoinManagerDelegate {
             self.btcLabel.text = bitcoin[0].shortPrice + self.curr
             self.ethLabel.text = bitcoin[1].shortPrice + self.curr
             self.xrpLabel.text = bitcoin[2].shortPrice + self.curr
+            self.activityView.stopAnimating()
               }
           }
     
     func didFailWithError(error: Error) {
         print(error)
+    }
+    
+     func showActivityIndicatory() {
+        activityView.color = .darkGray
+        activityView.center = self.view.center
+        self.view.addSubview(activityView)
+        activityView.startAnimating()
     }
 }
     
